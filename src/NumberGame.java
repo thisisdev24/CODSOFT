1b4bfd3a5ab7615d40db85efb6d97417c4390198
Dev Khare <imdevkhare@gmail.com>
2025-06-06 12:30:05 +0530

    Add user's guess and random number generation

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class NumberGame {
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGame {
+
+    private int r_number, guess;
+
+    void userGuess() {
+        System.out.println("Enter your guess: ");
+        guess = new Scanner(System.in).nextInt();
+    }
+
+    void generateRandom() {
+        System.out.println("New Number Generated!");
+        r_number = new Random().nextInt(100) + 1;
+    }
 }
