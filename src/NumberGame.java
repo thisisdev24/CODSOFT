e107ef328b6f909343f7ae1fbc88682c812ca6ce
Dev Khare <imdevkhare@gmail.com>
2025-06-06 12:59:56 +0530

    Add check for guess and limits

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -3,15 +3,59 @@ import java.util.Random;
 
 public class NumberGame {
 
-    private int r_number, guess;
+    private int r_number, guess, attempts;
+    Scanner input;
+
+    NumberGame() {
+        r_number = 0;
+        guess = 0;
+        attempts = 0;
+        input = new Scanner(System.in);
+    }
 
     void userGuess() {
-        System.out.println("Enter your guess: ");
-        guess = new Scanner(System.in).nextInt();
+        System.out.println("You have 7 attempts in total. \nEnter your guess: ");
+        guess = input.nextInt();
+        attempts++;
+        if(attempts > 7) {
+            System.out.println("Uh-oh! Out of attempts.");
+            System.out.print("Restart Game? Y|N: \n");
+            String restart = input.nextLine();
+            if(restart.equals("Y")) {
+                generateRandom();
+            } else {
+                System.exit(0);
+            }
+        }
+        checkGuess();
     }
 
     void generateRandom() {
         System.out.println("New Number Generated!");
         r_number = new Random().nextInt(100) + 1;
+        userGuess();
+    }
+
+    void checkGuess() {
+        if(r_number == guess) {
+            System.out.println("Correct!");
+            System.out.print("Play again? Y|N: \n");
+            String repeat = input.nextLine();
+            if(repeat.equals("Y")) {
+                userGuess();
+            } else {
+                System.exit(0);
+            }
+        } else if (r_number > guess ) {
+            System.out.println("Too low! Try again.");
+            userGuess();
+        } else {
+            System.out.println("Too High! Try again. ");
+            userGuess();
+        }
+    }
+
+    public static void main(String... args) {
+
     }
 }
