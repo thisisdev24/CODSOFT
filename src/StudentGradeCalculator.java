064a1bc0635315cfcde0421ce8a35a31699a5f48
Dev Khare <imdevkhare@gmail.com>
2025-06-07 23:23:24 +0530

    Assign Grades

--- src/StudentGradeCalculator.java
+++ src/StudentGradeCalculator.java
@@ -27,4 +27,22 @@ public class StudentGradeCalculator {
     float avgPercentage() {
         return (float) total() / n_subs;
     }
+
+    String gradeAssignment() {
+        float percentage = avgPercentage();
+
+        if(percentage >= 85.00) {
+            return "A";
+        } else if (percentage >= 75.00 && percentage < 85.00) {
+            return "B";
+        } else if (percentage >= 65.00 && percentage < 75.00) {
+            return "C";
+        } else if (percentage >= 55.00 && percentage < 65.00) {
+            return "D";
+        } else if (percentage >= 45.00 && percentage < 55.00) {
+            return "E";
+        } else {
+            return "F";
+        }
+    }
 }
