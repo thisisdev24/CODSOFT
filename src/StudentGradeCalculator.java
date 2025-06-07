4745642572565512d415a4ab2210b260cecb4a02
Dev Khare <imdevkhare@gmail.com>
2025-06-07 23:12:26 +0530

    Take user input of marks

--- src/StudentGradeCalculator.java
+++ src/StudentGradeCalculator.java
@@ -2,4 +2,22 @@ import java.util.Scanner;
 
 public class StudentGradeCalculator {
 
+    private int math, physx, chem, english, hindi;
+
+    void marksInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter marks obtained (out of 100) in each subject.");
+        System.out.print("Mathematics: ");
+        math = sc.nextInt();
+        System.out.print("\n\nPhysics: ");
+        physx = sc.nextInt();
+        System.out.print("\n\nChemistry: ");
+        chem = sc.nextInt();
+        System.out.print("\n\nEnglish: ");
+        english = sc.nextInt();
+        System.out.print("\n\nHindi: ");
+        hindi = sc.nextInt();
+    }
+
+
 }
