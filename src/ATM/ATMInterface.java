b42ef217896dca361bbb9402a2851d438e25c8cf
Dev Khare <imdevkhare@gmail.com>
2025-06-09 01:27:06 +0530

    Add main method

--- src/ATM/ATMInterface.java
+++ src/ATM/ATMInterface.java
@@ -1,18 +1,63 @@
 package ATM;
 
+import java.util.Scanner;
+
 public class ATMInterface extends BankAccount {
 
     void withdraw(double amount) {
         if(balance >= amount) {
             balance -= amount;
+            System.out.println("Successfully withdrawn!");
+        } else {
+            System.out.println("Not sufficient balance!");
         }
     }
 
     void deposit(double amount) {
         balance += amount;
+        System.out.println("Successfully deposited!");
     }
 
     double checkBalance() {
         return balance;
+    }
+
+    void userInterface() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Deposit Cash");
+        System.out.println("2. Withdraw Cash");
+        System.out.println("3. Check Account Balance");
+        System.out.println("4. Exit");
+        System.out.print("\nEnter your choice: ");
+        short choice = sc.nextShort();
+
+        double amount = 0;
+        switch (choice) {
+            case 1 -> {
+                System.out.print("\nEnter amount (in $): ");
+                amount = sc.nextDouble();
+                System.out.println("Depositing $" + amount + " ...");
+                deposit(amount);
+                userInterface();
+            }
+            case 2 -> {
+                System.out.print("\nEnter amount (in $): ");
+                amount = sc.nextDouble();
+                System.out.println("Withdrawing $" + amount + " ...");
+                withdraw(amount);
+                userInterface();
+            }
+            case 3 -> {
+                System.out.println("Current Account balance: $" + checkBalance());
+                userInterface();
+            }
+            default -> System.exit(0);
+        }
+    }
+
+    public static void main(String... args) {
+        ATMInterface atm = new ATMInterface();
+        System.out.println("Welcome to DBI ATM!");
+        atm.userInterface();
     }
 }
